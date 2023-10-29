04d94e983bbf2e53e527c8531a7594f3f3178cff
rhuallpa <rebeca.huallpa@tecsup.edu.pe>
2023-10-29 13:52:45 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +6,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'appAngular';
+  nombre = 'Rebeca Soledad Huallpa Centeno';
+  edad = 20;
+  email = 'rebeca.huallpa@gmail.com';
+  sueldos = [1700, 1600, 1900];
+  activo = true;
+  sitio='http://www.google.com';
+
+  profesion='';
+  anios='';
+
+  contador=1;
+  incrementar(){
+    this.contador++;
+  }
+
+  decrementar(){
+    this.contador--;
+  }
+
+  esActivo() {
+    if (this.activo) {
+      return 'Trabajador activo';
+    } else {
+      return 'Trabajador inactivo';
+    }
+  }
+
+  ultimos3Sueldos() {
+    let suma = 0;
+    for (let x = 0; x < this.sueldos.length; x++) {
+      suma += this.sueldos[x];
+    }
+    return suma;
+  }
 }
+
+
+
+  
